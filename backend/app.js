0f4b1666b5e42b524e6056dfa24d4e717dec36bb
charlieolinsky <98860404+charlieolinsky@users.noreply.github.com>
2022-07-18 21:44:34 -0400

    Setup2

--- backend/app.js
+++ backend/app.js
@@ -6,15 +6,16 @@ const app = express();
 const port = 5000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send('Backend Running')
 })
 
 app.get('/getRandomUsers', (req, res) => {
-    request('https://randomuser.me/api/?results=1', function(error, response, body) {
+    request('https://randomuser.me/api/?inc=name', function(error, response, body) {
         if(!error && response.statusCode == 200) {
             var parsedBody = JSON.parse(body)
             var userInfo = parsedBody["results"]
-            res.send({userInfo}); 
+            res.send({userInfo});
+            console.log(userInfo) 
            
         }
     })
--- frontend/src/RandomUser.js
+++ frontend/src/RandomUser.js
@@ -1,12 +1,30 @@
-import React, { useState } from 'react';
+import React, { Component } from 'react';
+import axios from 'axios'
 
+export default class RandomUser extends Component {
+    constructor() {
+        super();
+        this.state = {
+            info: "N/A"
+        }
+    }
 
-const RandomUser = () => {
-    const [info, setInfo] = useState(); 
-    return(
-        <div>
-            <p>User Info: {info} </p>
-        </div>
-    )
+    componentDidMount = () => {
+        axios.get("/getRandomUsers").then(response => {
+            this.setState({
+                info : response.data.userInfo
+            })
+        })
+    };
+
+    render() {
+        return(
+            <div>
+                <button onClick={this.componentDidMount}>Get User Info</button>
+                <p>User Info: {this.state.info} </p>
+            </div>
+        );
+    }
+
 }
-export default RandomUser; 
+    
