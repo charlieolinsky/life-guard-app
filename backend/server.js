466edf72c5f04b853cb4261115a05556d0d452d6
charlieolinsky <98860404+charlieolinsky@users.noreply.github.com>
2022-07-23 22:27:54 -0400

    Added to Department
    worked on serverside, almost functional

--- backend/server.js
+++ backend/server.js
@@ -29,7 +29,7 @@ server.get('/getRandomUsers', async (req, res) => {
 						const parsedData = [result]
             
             res.send({parsedData});
-            console.log("User Sent to Front") 
+            //console.log("User Sent to Front") 
            
         }
     })
@@ -37,24 +37,33 @@ server.get('/getRandomUsers', async (req, res) => {
 
 server.post("/insertUser", (req, res) => {
   
-  console.log(req.body.firstName)
+  console.log(req.body.department)
   res.redirect("http://localhost:3000")
+  console.log("User Called")
 
+  //Insert User
   connection.query(`INSERT INTO users(username, hashPassword, firstName, lastName, picture) VALUES('${req.body.userName}', '${req.body.hashPassword}', '${req.body.firstName}', '${req.body.lastName}', '${req.body.pictureURL}')`, (error, results) => {
-      if (error) throw error
-      console.log(results)
+     
+  //Add User Department
+  // INSERT INTO department (<>) SELECT (firstName, lastName) FROM users
+    
+    if (error) throw error
   })
 })
 
 server.post("/selectDepartment", (req, res) => {
     res.redirect("http://localhost:3000")
+    console.log("Department Called")
+    console.log(res.body.department)
+    //connection.query(`SELECT firstName, lastName INTO department FROM users`)
 
-    connection.query(`SELECT firstName, lastName INTO department FROM users`)
+   
+
     if (error) throw error
       console.log(results)
 })
 
 
 server.listen(port, () => {
-  console.log(`app listening on port ${port}`)
+  console.log(`server listening on port ${port}`)
 })
--- frontend/src/CreateUser.js
+++ frontend/src/CreateUser.js
@@ -10,12 +10,13 @@ export default class CreateUser extends Component {
             lastName: 'N/A',
             userName: "N/A",
             hashPassword: "N/A",
-            pictureURL: "N/A"
+            pictureURL: "N/A",
+            department: "N/A"
         };
     }
 
-    getUserFromAPI = () => {
-        axios.get("/getRandomUsers").then(response => {
+    getUserFromAPI = async () => {
+        await axios.get("/getRandomUsers").then (response => {
             this.setState({
                 firstName : response.data.parsedData[0]['name']['first'],
                 lastName : response.data.parsedData[0]['name']['last'],
@@ -24,13 +25,11 @@ export default class CreateUser extends Component {
                 pictureURL: response.data.parsedData[0]['picture']['large']
                 
             })
-        }).then(this.fillRandomForm())
-
-        
+        })
+        this.fillRandomForm()
     };
 
     fillRandomForm = () => {
-
         var fName = document.getElementById('fName');
         fName.value = this.state.firstName
         var lName = document.getElementById('lName');
@@ -41,48 +40,76 @@ export default class CreateUser extends Component {
         pWord.value = this.state.hashPassword
     }
 
+    setUserDepartment = () => {
+        var isAD = document.getElementById('ad').checked;
+        var isEHLG = document.getElementById('ehlg').checked;
+        var isHLG = document.getElementById('hlg').checked;
+        var isLG = document.getElementById('lg').checked;
+
+        if(isAD){
+            
+            this.setState(() => {
+                return {department: 'aquatics_director'}
+            })
+        }
+        else if(isEHLG){
+            this.setState(() => {
+                return {department: 'executive_head_lifeguard'}
+            })
+        }
+        else if(isHLG){
+            this.setState(() => {
+                return {department: 'head_lifeguard'}
+            })
+        }
+        else if(isLG){
+            this.setState(() => {
+                return {department: 'lifeguard'}
+            })
+        }
+        else{
+            this.setState(() => {
+                return {department: "No Department Selected"}
+            })
+        }
+    }
+
     render() {
         return(
             <div className='sign-up'>
                 <button onClick={() => this.getUserFromAPI()}>Generate Random User</button>
-                
-                    <form className='sign-up-form' action='http://localhost:5000/insertUser' method='POST'>
-                        <input type='text' name='firstName'placeholder='First Name' id='fName'/>
-
-
-                        <input type='text' name='lastName' placeholder='Last Name' id='lName'/>
-                        <input type='text' name='userName' placeholder='Username' id='uName'/>
-                        <input type='text' name='hashPassword' placeholder='Password' id='pWord'/>
-                        <input type='text' name='pictureURL' value='pictureURL' id='picURL'/>
                     
-                    <div className='select-department'>
-                        <fieldset>
-                            <legend>User Job title: </legend>
-                            <div>
-                                <input type="radio" id="ad" name="job_title" value="Aquatics Director"/>
-                                <label for="ad"> Aquatics Director</label>
-                            </div>
+                        <form className='sign-up-form' action='http://localhost:5000/insertUser' method='POST'>
+                            
+                            <input type='text' name='firstName'placeholder='First Name' id='fName'/>
+                            <input type='text' name='lastName' placeholder='Last Name' id='lName'/>
+                            <input type='text' name='userName' placeholder='Username' id='uName'/>
+                            <input type='text' name='hashPassword' placeholder='Password' id='pWord'/>
+                            <input type='text' name='pictureURL' /*value='pictureURL'*/ id='picURL'/>       
 
-                            <div>
-                                <input type="radio" id="ehlg" name="job_title" value="Executive Head Lifeguard"/>
-                                <label for="ehg"> Executive Head Lifeguard</label>
-                            </div>
-
-                            <div>
-                                <input type="radio" id="hlg" name="job_title" value="Head Lifeguard"/>
-                                <label for="hlg"> Head Lifeguard</label>
-                            </div>
-
-                            <div>
-                                <input type="radio" id="lg" name="job_title" value="Lifeguard"/>
-                                <label for="lg"> Lifeguard</label>
-                            </div>   
-                                
-                        </fieldset>
-                    </div>        
-
-                        <input type='submit' value='Submit' />
-                    </form>
+                            <fieldset>
+                                    <legend>User Job title: </legend>
+                                    <div>
+                                        <input type="radio" id="ad" name="job_title" />
+                                        <label htmlFor="ad"> Aquatics Director</label>
+                                    </div>
+                                    <div>
+                                        <input type="radio" id="ehlg" name="job_title" />
+                                        <label htmlFor="ehg"> Executive Head Lifeguard</label>
+                                    </div>
+                                    <div>
+                                        <input type="radio" id="hlg" name="job_title" />
+                                        <label htmlFor="hlg"> Head Lifeguard</label>
+                                    </div>
+                                    <div>
+                                        <input type="radio" id="lg" name="job_title" />
+                                        <label htmlFor="lg"> Lifeguard</label>
+                                    </div>   
+                                        
+                            </fieldset>
+                            
+                            <input type='submit' value='Submit' />
+                        </form>
             </div>
         );
     }
